33aea5df0064088d3e295243c3923b391b1d099c
Omar Kada <kadaomar@hotmail.com>
2026-03-03 17:24:10 +0100

    chore: Fix formatting of import statement in eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import {defineConfig, globalIgnores} from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
