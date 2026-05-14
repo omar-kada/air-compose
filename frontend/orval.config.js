adf9857b075325514a5111b86b79c441a077003a
Omar Kada <kadaomar@hotmail.com>
2026-05-14 19:38:33 +0200

    build(deps): explicit httpClient to be axios in orval configuration

--- frontend/orval.config.js
+++ frontend/orval.config.js
@@ -1,9 +1,15 @@
-module.exports = {
+/**
+ * @type {import('orval/dist/config').Config}
+ */
+const config = {
   airCompose: {
     input: '../api/tsp-output/schema/openapi.1.0.yaml',
     output: {
       target: './src/api/api.ts',
       client: 'react-query',
+      httpClient: 'axios',
     },
   },
 };
+
+module.exports = config;
