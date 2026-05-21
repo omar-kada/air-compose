7a4a9d27a3c5b24f9b894f657607e537af7c098a
Omar Kada <kadaomar@hotmail.com>
2026-05-21 17:17:56 +0200

    chore: add max width to page

--- frontend/src/components/view/header-layout.tsx
+++ frontend/src/components/view/header-layout.tsx
@@ -3,10 +3,10 @@ import { Separator } from '../ui/separator';
 
 export function HeaderLayout({ header, children }: { header: ReactNode; children: ReactNode }) {
   return (
-    <>
-      {header && <div className="p-2 px-4">{header}</div>}
+    <div className="flex flex-col h-full">
+      {header && <div className="py-2">{header}</div>}
       {header && <Separator orientation="horizontal" />}
       <div className="flex flex-col flex-1 overflow-hidden">{children}</div>
-    </>
+    </div>
   );
 }
